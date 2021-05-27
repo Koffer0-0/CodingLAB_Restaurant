03472f37a171e8bf33cd0fe9b428dca1a9ce910a
Koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2021-05-27 14:06:43 +0600

    Update panel.php

--- Admin/panel.php
+++ Admin/panel.php
@@ -1,7 +1,19 @@
 
 <main class = "row m-3 py-3">
     <section class = "block1 col-md-7 col-sm-12 p-2 m-md-2 m-sm-2 ">
+     <?php
+        require_once ('../Database/ShowOOP.php');
+        $obj = new Show();
+        $reciepe = $obj->select_table('recieps');
+        for ($i = 0; $i < count($reciepe); $i++) {
+     ?>
+       <section class = "item">
+           <tr>
+               <td> <?php echo $reciepe[$i][3] ?></td>
+           </tr>
+       </section>
 
+     <?php } ?>
     </section>
     <section class = "block2 col-md-4 col-sm-12 p-2 m-md-2 m-sm-2">
         some text must be here
